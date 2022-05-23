9d8f82fc4b9752295234d1d3886982e24c38d347
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-05-23 12:46:57 +0530

    logic to prevent access to homepage if not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import SignupDemo from './pages/SignUpDemo';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
+
+  const WithRouter = () =>{
+    const nevigate = useNavigate()
+
+    useEffect(()=>{
+      const user = JSON.parse(localStorage.getItem("user"))
+      if(!user)
+        nevigate("/login")
+    }, [])
+
+      return(
+        <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/register' element={<Signup />} />
         <Route exact path='/registerdemo' element={<SignupDemo />} />
       </Routes>
+      )
+  } 
+
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <WithRouter />
     </BrowserRouter>
   );
 }
